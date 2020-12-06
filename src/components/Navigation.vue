29eddc4f44dcc2d7380c38c2bc196b9731643af9
İnanç Dağyolu <inanc@sitorbis.com>
2020-12-06 21:52:48 +0300

    Hata Ayıklandı

---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav class="navbar">
+      <div class="navbar-logo">
+        <img src="../assets/img/logotravel.png" alt="">
+      </div>
+      <div class="collapse">
+        <ul class="navbar-nav">
+          <li class="navbar-nav__item">
+            <router-link to="/" class="navbar-nav__link">Gidilecek Yerler</router-link>
+          </li>
+          <li class="navbar-nav__item">
+            <router-link to="/about" class="navbar-nav__link">Blog</router-link>
+          </li>
+        </ul>
+        <div class="userbar">
+          <a href="" class="btn btn-dark">Üye Ol</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/navigation";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navigation></Navigation>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navigation from "@/components/Navigation";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Navigation
   }
 };
 </script>
+
+<style lang="scss">
+  @import "../assets/scss/home";
+</style>
